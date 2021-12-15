221fe8880c27535737e1e6b584a561ae7d3854da
aidian3k <93425971+aidian3k@users.noreply.github.com>
2021-12-15 11:11:18 +0100

    Added Gauss  elimination to the project

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,26 @@
 #include "gauss.h"
+#include <math.h>
+#include <stdio.h>
+int eliminate(Matrix *mat, Matrix *b)
+{
+        double **A=mat->data;
+        double **B=b->data;
+        int w=mat->r;
+        int k=mat->c;
+        for(int i=0;i<k-1;i++)
+        {
+		for(int j=i+1;j<w;j++)
+                {
+                        if(A[i][i]!=0)
+                        {
+                                double q=A[j][i]/A[i][i];
+                                for(int z=0;z<k;z++) A[j][z]-=q*A[i][z];
+                                B[j][0]-=q*B[i][0];
+                        }
+                        else return 1;
+                }
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+        }
+        return 0;
 }
 
