9c68f887ed42d304b9b51a54a65e43f2d6eafc23
CezaryS8 <czarek.skorupski2001@gmail.com>
2021-12-15 11:09:26 +0100

    implementation of backsubscribin

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,29 @@
 #include "backsubst.h"
-/**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int  backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+	double **A=mat->data;
+	double **B=b->data;
+	double **ans=x->data;
+	int w=mat->r;
+	int k=mat->c;
+	int pom=0, i, j;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	if(w != b->r || b->c != 1 || w != k)
+		return 2;
+	for(i=w-1;i>=0;i--)
+	{
+		double sum=0;
+		int pom1=0;
+		for(j=k-1;j>k-1-pom;j--)
+		{
+			sum+=A[i][j]*ans[pom1][0];
+			pom1++;
+		}
+		if(A[i][j]==0) return 1;
+		ans[pom][0]=(B[i][0]-sum)/(A[i][j]);
+		pom++;
+	}
+	return 0;
 }
 
 
